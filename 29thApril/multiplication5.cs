f8e64768e9a4778fd7e5645694f0961193d6db94
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-29 17:03:19 +0530

    commit

---
+++ 29thApril/multiplication5.cs
@@ -0,0 +1,18 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        
+        for(int i=1;i<=10;i++){
+            int n=5;
+            Console.WriteLine (n*i);
+        
+        }
+        
+    }
+}
