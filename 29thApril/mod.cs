d0058caded9e9dcede2454b8ce15b4b65088fdb5
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-29 15:35:50 +0530

    commit

---
+++ 29thApril/mod.cs
@@ -0,0 +1,22 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    static int mod(int a,int b){
+        return a%b;
+    
+    }
+    
+     static void Main()
+    
+    {
+        int num1=20;
+        int num2=10;
+        
+        Console.WriteLine("Multiplication: "+mod(num1,num2));
+        
+    }
+}
