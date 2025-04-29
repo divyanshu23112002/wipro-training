6857cd0c66bb954523d5e5ea412d2ac6c753c530
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-29 15:35:06 +0530

    commit

---
+++ 29thApril/divide.cs
@@ -0,0 +1,22 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    static int div(int a,int b){
+        return a/b;
+    
+    }
+    
+     static void Main()
+    
+    {
+        int num1=20;
+        int num2=10;
+        
+        Console.WriteLine("Divide: "+div(num1,num2));
+        
+    }
+}
