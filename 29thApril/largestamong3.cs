53cbb148b436653b7512af7e87cb55db20f8a631
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-29 17:18:21 +0530

    commit

---
+++ 29thApril/largestamong3.cs
@@ -0,0 +1,20 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int a=10;
+        int b=5;
+        int c=20;
+        
+       
+            Console.WriteLine (Math.Max(a,Math.Max(b,c)));
+        
+        
+        
+    }
+}
