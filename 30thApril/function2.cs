efa56b6eb5831a9597d46068844a489815092107
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-30 12:51:46 +0530

    commit

---
+++ 30thApril/function2.cs
@@ -0,0 +1,16 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    static void user(string name){
+        Console.WriteLine("Hello "+name);
+    }
+     static void Main()
+    {
+        user("DJ");
+       
+    }
+}
