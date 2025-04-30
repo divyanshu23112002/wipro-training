0e9d8704543f83df79d912cbf4d4917f73dbf049
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-30 12:48:45 +0530

    commit

---
+++ 30thApril/squarefunction.cs
@@ -0,0 +1,17 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    static int square(int num){
+        return num*num;
+    }
+     static void Main()
+    {
+        int res=square(5);
+        Console.WriteLine("Square is :" +res);
+       
+    }
+}
