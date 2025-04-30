7f92a5e6432b436fbb4fdba2f723ea42e4e16c30
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-30 09:58:53 +0530

    commit

---
+++ 30thApril/grid.cs
@@ -0,0 +1,20 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+       for(int row=1;row<=3;row++){
+           for(int col=1;col<=3;col++){
+               Console.Write(col+"| ");
+           }
+           Console.WriteLine();
+       }
+         
+        
+        
+    }
+}
