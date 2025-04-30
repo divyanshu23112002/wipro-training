77392f46ff20d2a801760fcfc187eb70bb0c0734
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-30 09:05:35 +0530

    commit

---
+++ 30thApril/grade.cs
@@ -0,0 +1,18 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int marks=78;
+        if(marks>=90)Console.WriteLine ("Grade A");
+        else if(marks>=75)Console.WriteLine ("Grade B");
+        else if(marks>=60)Console.WriteLine ("Grade C");
+        else Console.WriteLine ("Grade D");
+        
+        
+    }
+}
