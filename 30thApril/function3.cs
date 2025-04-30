a702295d4b2be34600a218f44bf36cab4165c1a3
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-30 12:57:19 +0530

    commit

---
+++ 30thApril/function3.cs
@@ -0,0 +1,16 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    static void printmsg(){
+        Console.WriteLine("Hello,  Welcome to Chat GPT ");
+    }
+     static void Main()
+    {
+        printmsg();
+       
+    }
+}
