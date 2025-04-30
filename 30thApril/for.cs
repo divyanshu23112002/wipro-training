d0b35c516b26139a014482ccbe63d1d34216f562
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-30 09:22:47 +0530

    commit

---
+++ 30thApril/for.cs
@@ -0,0 +1,15 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        for(int i=1;i<=5;i++)Console.WriteLine(i);
+         
+        
+        
+    }
+}
