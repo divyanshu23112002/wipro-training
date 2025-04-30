eb9c29aa886250d1d81a8f7a031d858758671100
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-30 09:49:01 +0530

    commit

---
+++ 30thApril/dowhile.cs
@@ -0,0 +1,19 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int i=1;
+      do {
+            Console.WriteLine(i);
+            i++;
+        } while(i<=5);
+         
+        
+        
+    }
+}
