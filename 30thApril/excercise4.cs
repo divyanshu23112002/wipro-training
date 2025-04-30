7b089bfa2693e43c36a27bd828686431557c6625
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-30 10:16:57 +0530

    commit

---
+++ 30thApril/excercise4.cs
@@ -0,0 +1,16 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int i=1;
+        while(i*i<=100){
+            Console.WriteLine("Square of "+i+" = "+(i*i));
+            i++;
+        }
+    }
+}
