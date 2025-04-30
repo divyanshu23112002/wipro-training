d2c596acd096added59a9a7283907bb3d63ccb3b
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-04-30 09:16:46 +0530

    commit

---
+++ 30thApril/switch.cs
@@ -0,0 +1,25 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int day=3;
+        switch(day){
+            case 1: Console.WriteLine ("Monday");
+            break;
+            case 2: Console.WriteLine ("Tuesday");
+            break;
+            case 3: Console.WriteLine ("Wednesday");
+            break;
+            default: Console.WriteLine ("Invalid");
+            break;
+        }
+         
+        
+        
+    }
+}
