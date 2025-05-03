b3d625b5572b4d67bc93006fe78c28969edbcd99
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-05-03 13:01:24 +0530

    my

--- 03rdMay/Employee_Curd_Operation.sql
+++ 03rdMay/Employee_Curd_Operation.sql
@@ -1,1 +1,11 @@
-//my first file
+//my first file in sql
+
+create table Employees(
+EmployeeID int primary key,
+FirstName varchar(50),
+LastName varchar(50),
+Department varchar(50),
+salary decimal(10,2),
+HireDate date
+);
+
