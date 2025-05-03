eb17124a595eac9eb0720ccf232a8ea856de88f3
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-05-03 13:16:33 +0530

    commit

--- 03rdMay/Employee_Curd_Operation.sql
+++ 03rdMay/Employee_Curd_Operation.sql
@@ -9,3 +9,21 @@ salary decimal(10,2),
 HireDate date
 );
 
+Insert into Employees(EmployeeID,
+FirstName,
+LastName,
+Department,
+salary,
+HireDate)
+
+values
+(1,'Rajan','Kumar','Biology',45087.45,'2023-11-22'),
+(2,'Santosh','Kumar','Chemistry',49087.40,'2021-01-12'),
+(3,'Ajay','Verma','Mathematics',50087.50,'2022-02-02');
+
+
+select * from emloyees; 
+
+select firstname, department from employees where salary>47000;
+
+select firstname,salary from employees  where Department='Mathematics';
