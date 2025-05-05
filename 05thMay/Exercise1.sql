1dc5261fa689315dd5634f3e7c70b045c0c23934
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-05-05 10:29:31 +0530

    commit

---
+++ 05thMay/Exercise1.sql
@@ -0,0 +1,5 @@
+create Procedure getProductRevenue @ProductID Int as begin 
+select p.ProductID,p.ProductName , sum(o.Quantity * p.price) as TotalRevenue from orders o inner join
+products p ON o.ProductID=p.ProductID where o.ProductID=@ProductID group by p.ProductID,p.ProductName END;  
+
+exec getProductRevenue @productID=1;
