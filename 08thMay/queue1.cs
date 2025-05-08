25e6dd7b3176b3dd4851d2998f646250dc7524bd
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-05-08 14:40:59 +0530

    commit

---
+++ 08thMay/queue1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        //creating a queue of string
+        Queue<string> customers = new Queue<string>();
+
+        //Enqueue Elements 
+       customers.Enqueue("Amit");
+        customers.Enqueue("Neha");
+        customers.Enqueue("Ravi");
+
+        Console.WriteLine("Serving Elements : ");
+        while(customers.Count>0)Console.WriteLine("Serving : "+customers.Dequeue());
+       
+
+
+    }
+}
