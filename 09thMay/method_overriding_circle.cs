a7dec32d32508b34576d71929ba85294c3483613
Divyanshu Shekhar <6shekhar7@gmail.com>
2025-05-09 16:15:07 +0530

    commit

---
+++ 09thMay/method_overriding_circle.cs
@@ -0,0 +1,23 @@
+using System;
+class Shape
+{
+    public virtual void Draw()
+    {
+        Console.WriteLine("Drawing a shape");
+    }
+}
+class Circle:Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a Circle");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Shape s=new Circle();
+        s.Draw();
+    }
+}
