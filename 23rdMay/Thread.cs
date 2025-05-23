a3ce5e873ece71463217606e2504e62ab50bb63c
Divyanshu <6shekhar7@gmail.com>
2025-05-23 10:28:43 +0530

    commit

--- 23rdMay/Thread.cs
+++ 23rdMay/Thread.cs
@@ -2,22 +2,34 @@ using System;
 using System.Threading;
 class Program
 {
-    static void PrintNumber()
+    /*
+    static void Printmsg(object meassage)
     {
-        for (int i = 1; i <= 5; i++)
-        {
-            Console.WriteLine($"Thread: {i}"); 
-            Thread.Sleep(500);  //pause for 0.5 second
-        }
+        // for (int i = 1; i <= 5; i++)
+        // {
+        //     Console.WriteLine($"Thread: {i}"); 
+        //     Thread.Sleep(500);  //pause for 0.5 second
+        // }
+        string msg = (string)meassage; //Cast to correct type
+        Console.WriteLine("Meassage : " + msg);
+
     }
+    */
     static void Main(string[] args)
     {
-        Thread myThread=new Thread(PrintNumber);  //Create a thread
-        myThread.Start();   //Start the thread
-        for (int i = 1;i <= 5;i++)
-        {
-            Console.WriteLine($"Main: {i}");
-            Thread.Sleep(500);
-        }
+        // Thread myThread=new Thread(PrintNumber);  //Create a thread
+        // myThread.Start();   //Start the thread
+        // for (int i = 1;i <= 5;i++)
+        // {
+        //     Console.WriteLine($"Main: {i}");
+        //     Thread.Sleep(500);
+        // }
+        //Thread thread = new Thread(new ParameterizedThreadStart(Printmsg));
+        //thread.Start("Hello from the thread!");
+
+        int a = 5, b = 10;
+        Thread thread = new Thread(() =>
+        { Console.WriteLine($"Sum: {a + b}"); });
+        thread.Start();
     }
 }
